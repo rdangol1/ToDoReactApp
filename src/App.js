8b6e702e872bcb491bfb4c0248b0a1089bf0e2ab
Ridhisha Dangol <ridhishadangol@pingidentity.com>
2021-06-04 11:53:38 -0600

    adding html and css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 const App = () => {
   return (
     <div className="contatiner py-20 mx-auto max-w-md">
-      <div className="bg-white rounded-lg p-10 text-black shadow"></div>
+      <div className="bg-white rounded-lg p-10 text-black shadow">
+      <div className="border-b-2 p-2 ">
+          <input className=" mr-5" type="checkbox" id="first"/>
+          <label className="text-gray-700" id="first">finish to do list</label>
+          </div>
+        <div className="border-b-2 p-2 ">
+          <input className=" mr-5" type="checkbox" id="first"/>
+          <label className="text-gray-300 line-through " id="first">finish to do list</label>
+          </div>
+
+      </div>
     </div>
   );
 };
