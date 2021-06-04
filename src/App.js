210db1db324e60bec1222c39543f039d5101399f
Noah Buscher <noahbuscher@pingidentity.com>
2021-06-04 09:43:09 -0700

    Add tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contatiner py-20 mx-auto max-w-md">
+      <h1 className="text-lg text-gray-800">Hello!</h1>
     </div>
   );
-}
+};
 
 export default App;
