246a3591836e168684d47bff312cd7ee9f497a6c
Ridhisha Dangol <ridhishadangol@pingidentity.com>
2021-06-10 14:48:52 -0600

    more icons

--- src/App.js
+++ src/App.js
@@ -205,7 +205,9 @@ const App = () => {
         </div>
 
         <button onClick={sortItems} className="m-1">
-          Sort Latest
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 4h13M3 8h9m-9 4h9m5-4v12m0 0l-4-4m4 4l4-4" />
+        </svg>
         </button>
 
         <div className={classFeatures}>Yay!! you are all done</div>
